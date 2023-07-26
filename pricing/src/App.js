0cb5053dd3e84ad0ff910315c31c382c509023f8
ryanp <ryanpanimation@gmail.com>
2023-07-25 22:40:43 -0500

    card setup

--- pricing/src/App.js
+++ pricing/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main className="App">
+      Our Pricing Annually Monthly
+      <div className="cardHolder">
+        <div className="card">
+          Basic <h1>$19.99</h1> <h1>$199.99</h1> <span>500 GB Storage</span>
+          <span> 2 Users Allowed</span> <span>Send up to 3 GB</span>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          Professional <h1>$24.99</h1>
+          <h1>$249.99</h1> <span> 1 TB Storage</span>
+          <span> 5 Users Allowed</span>
+          <span> Send up to 10 GB</span>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          Master <h1>$39.99</h1> <h1>$399.99</h1> <span>2 TB Storage</span>
+          <span>10 Users Allowed</span> <span>Send up to 20 GB</span>
+          <button>Learn More</button>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
-    </div>
+        . Coded by <a href="#">Ryan Paulson</a>.
+      </div>
+    </main>
   );
 }
 
