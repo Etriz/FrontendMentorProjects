b70e5ec72b7d3cc2d1ef5360244a62d223fd4ead
ryanp <ryanpanimation@gmail.com>
2023-07-27 23:05:57 -0500

    card styling

--- pricing/src/App.js
+++ pricing/src/App.js
@@ -9,29 +9,40 @@ function App() {
   };
   return (
     <main className="App">
-      Our Pricing Annually
-      <label className="switch">
-        <input type="checkbox" onClick={() => handleToggle()} />
-        <span className="slider round"></span>
-      </label>
-      Monthly
+      <h1>Our Pricing</h1>
+      <p>
+        Annually
+        <label className="switch">
+          <input type="checkbox" onClick={() => handleToggle()} />
+          <span className="slider round"></span>
+        </label>
+        Monthly
+      </p>
       <div className="cardHolder">
         <div className="card">
-          Basic <h1>$19.99</h1> <h1>$199.99</h1> <span>500 GB Storage</span>
-          <span> 2 Users Allowed</span> <span>Send up to 3 GB</span>
-          <button>Learn More</button>
+          <div className="cardInner">
+            Basic {toggleClick ? <h1>$19.99</h1> : <h1>$199.99</h1>}
+            <span>500 GB Storage</span>
+            <span> 2 Users Allowed</span> <span>Send up to 3 GB</span>
+            <button>Learn More</button>
+          </div>
+        </div>
+        <div className="card mid">
+          <div className="cardInner">
+            Professional {toggleClick ? <h1>$24.99</h1> : <h1>$249.99</h1>}
+            <span> 1 TB Storage</span>
+            <span> 5 Users Allowed</span>
+            <span> Send up to 10 GB</span>
+            <button>Learn More</button>
+          </div>
         </div>
         <div className="card">
-          Professional <h1>$24.99</h1>
-          <h1>$249.99</h1> <span> 1 TB Storage</span>
-          <span> 5 Users Allowed</span>
-          <span> Send up to 10 GB</span>
-          <button>Learn More</button>
-        </div>
-        <div className="card">
-          Master <h1>$39.99</h1> <h1>$399.99</h1> <span>2 TB Storage</span>
-          <span>10 Users Allowed</span> <span>Send up to 20 GB</span>
-          <button>Learn More</button>
+          <div className="cardInner">
+            Master {toggleClick ? <h1>$39.99</h1> : <h1>$399.99</h1>}
+            <span>2 TB Storage</span>
+            <span>10 Users Allowed</span> <span>Send up to 20 GB</span>
+            <button>Learn More</button>
+          </div>
         </div>
       </div>
       <div class="attribution">
