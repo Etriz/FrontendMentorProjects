53d6275e263ca7183878f5cb0caeba7f0fb3cb57
ryanp <ryanpanimation@gmail.com>
2023-07-30 15:27:50 -0500

    countdown calcs

---
+++ countdown/app/countdown.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+
+const countdown = () => {
+  let countdownTo = new Date('July 1, 2024').getTime();
+  let [countdownDisplay, setCountdownDisplay] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  let x = setInterval(function (): void {
+    let dateNow = Date.now();
+    let timeLeft = countdownTo - dateNow;
+
+    let days: number = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours: number = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes: number = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds: number = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    setCountdownDisplay({ days: days, hours: hours, minutes: minutes, seconds: seconds });
+  }, 1000);
+  return { countdownDisplay };
+};
+
+export default countdown;
