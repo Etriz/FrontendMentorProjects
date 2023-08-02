a0e443a85946f0f6b0f40275b12d25ea2023eb0a
ryanp <ryanpanimation@gmail.com>
2023-08-01 22:40:20 -0500

    starting page layout

---
+++ space/components/nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const nav = () => {
+  return (
+    <nav>
+      <Link href="">00 Home</Link>
+      <Link href="">01 Destination</Link>
+      <Link href="">02 Crew</Link>
+      <Link href="">03 Technology</Link>
+    </nav>
+  );
+};
+
+export default nav;
