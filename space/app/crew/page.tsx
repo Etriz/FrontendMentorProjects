99c37ffdc869b40d3600542a7ad3955b4d6a322a
ryanp <ryanpanimation@gmail.com>
2023-08-05 00:11:19 -0500

    page routing

---
+++ space/app/crew/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Crew = () => {
+  return <div>This is the Crew page</div>;
+};
+
+export default Crew;
---
+++ space/app/destination/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Nav from '../components/nav';
+
+const Destination = () => {
+  return <div>This is the destination page</div>;
+};
+
+export default Destination;
