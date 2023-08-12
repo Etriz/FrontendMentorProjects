1f114959659e3dce897c2797b802d643ce801375
ryanp <ryanpanimation@gmail.com>
2023-08-11 23:24:48 -0500

    data and layout for destination cards

--- space/app/destination/page.tsx
+++ space/app/destination/page.tsx
@@ -3,7 +3,26 @@ import data from '../../design/data.json';
 import styles from './page.module.css';
 
 const Destination = () => {
-  return <div>This is the destination page</div>;
+  const showDestinations = () => {
+    // return <div>{data.destinations[0].name}</div>;
+    return data.destinations.map((planet) => {
+      return (
+        <div className={styles.card}>
+          <span>{planet.name}</span>
+          <hr />
+          {planet.description}
+          <br />
+          <br />
+          Distance: {planet.distance}
+        </div>
+      );
+    });
+  };
+  return (
+    <main className={styles.main}>
+      <div className={styles.cardContainer}>{showDestinations()}</div>
+    </main>
+  );
 };
 
 export default Destination;
