0c20a1d1f0de6214437f7465bcb5bdb94affed13
ryanp <ryanpanimation@gmail.com>
2023-08-11 22:17:36 -0500

    mobile first styling

--- space/app/destination/page.tsx
+++ space/app/destination/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Nav from '../components/nav';
+import data from '../../design/data.json';
+import styles from './page.module.css';
 
 const Destination = () => {
   return <div>This is the destination page</div>;
--- space/app/page.tsx
+++ space/app/page.tsx
@@ -5,9 +5,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        So, you want to travel to Space Let’s face it; if you want to go to space, you might as well
-        genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and
-        relax because we’ll give you a truly out of this world experience! Explore
+        So, you want to travel to
+        <br />
+        <span className={styles.spaceTitle}>Space</span>
+        <br />
+        Let’s face it; if you want to go to space, you might as well genuinely go to outer space and
+        not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly
+        out of this world experience!
       </div>
     </main>
   );
